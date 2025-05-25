32894c5e6b6bf168a93e7762b9e9ce4448d9103e
marce <germy.gonzales95@gmail.com>
2025-05-25 12:15:21 -0400

    creacion de la vista milista en views.py

--- mi_proy25/mio/views.py
+++ mi_proy25/mio/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
 
 # Create your views here.
+def milista(request):
+    nom = "marcelo aruquipa"
+    fecha = datetime.now()
+    return render(request, 'pagina.html', {'nom':nom,'fecha':fecha})

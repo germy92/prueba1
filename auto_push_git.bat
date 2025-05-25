7ea31b0afac21e9f260678b7e9761968b0f99164
marce <germy.gonzales95@gmail.com>
2025-05-25 13:13:51 -0400

    $mensaje dom. 25/05/2025 13:13:51,34

--- auto_push_git.bat
+++ auto_push_git.bat
@@ -1,5 +1,7 @@
 @echo off
 cd /d D:\lab_django
+echo "Escribe el mensaje del commit:"
+read mensaje
 git add .
-git commit -m "Auto-commit %date% %time%"
+git commit -m "$mensaje %date% %time%"
 git push origin main

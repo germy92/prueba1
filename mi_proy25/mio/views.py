b0c1a7aa17553ca73fac8aee902d543bbf314c8b
marce <germy.gonzales95@gmail.com>
2025-05-25 12:34:13 -0400

    modificacion de la view

--- mi_proy25/mio/views.py
+++ mi_proy25/mio/views.py
@@ -1,31 +1,8 @@
 from django.shortcuts import render, HttpResponse
-from datetime import datetime
+from datetime import datetime, date
 
 # Create your views here.
 def milista(request):
     nom = "marcelo aruquipa"
-    fecha = datetime.now()
+    fecha = date.today()
     return render(request, 'pagina.html', {'nom':nom,'fecha':fecha})
-
-def eliminar_publicacion(request, pk):
-    publicacion = get_object_or_404(Publicacion, pk=pk)
-
-    if publicacion.usuario != request.user:
-        return HttpResponseForbidden("No puedes eliminar esta publicación.")
-
-    if request.method == 'POST':
-        publicacion.delete()
-        return redirect('lista_publicaciones')
-
-    return render(request, 'blog/confirmar_eliminacion.html', {'publicacion': publicacion})
-
-def detalle_publicacion(request, pk):
-    publicacion = get_object_or_404(Publicacion, pk=pk)
-    comentarios = Comentario.objects.filter(publicacion=publicacion)
-    form_comentario = ComentarioForm() if request.user.is_authenticated else None
-    contexto={
-        'publicacion':publicacion,
-        'comentarios':comentarios,
-        'form_comentario':form_comentario,
-    }
-    return render(request, 'blog/detalle_publicacion.html',contexto)
